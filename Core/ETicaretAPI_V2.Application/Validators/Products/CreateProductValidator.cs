8a327cbe8f3752de356130041530ea1b412fbd3b
huseyin corakli <huseyincorakli@icloud.com>
2023-07-26 02:56:40 +0300

    Validation

---
+++ Core/ETicaretAPI_V2.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,34 @@
+﻿using ETicaretAPI_V2.Application.ViewModels.Products;
+using FluentValidation;
+
+namespace ETicaretAPI_V2.Application.Validators.Products
+{
+    public class CreateProductValidator:AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Ürün adı boş olmamalı!")
+                .MinimumLength(2)
+                .MaximumLength(150)
+                .WithMessage("Ürün adı 2 ile 150 karakter arasında olmalı!");
+
+            RuleFor(x => x.Price)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Fiyat boş olmamalı!")
+                .Must(price => price >= 0)
+                .WithMessage("Fiyat 0 veya büyük olmalı!");
+
+            RuleFor(x => x.Stock)
+                .NotEmpty()
+                .NotEmpty()
+                .WithMessage("Stok boş olmamalı!")
+                .Must(stock => stock >= 0)
+                .WithMessage("Stock değeri 0 veya büyük olmalı");
+
+        }
+    }
+}
--- Presentation/ETicaretAPI_V2.API/Program.cs
+++ Presentation/ETicaretAPI_V2.API/Program.cs
@@ -1,4 +1,7 @@
+using ETicaretAPI_V2.Application.Validators.Products;
+using ETicaretAPI_V2.Infrastructure.Filters;
 using ETicaretAPI_V2.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +10,9 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
     policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod().AllowCredentials()
 ));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options=>options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
